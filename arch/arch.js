e337e9c2ca1090d200ada15f3483a4549ea85735
edgist <sern@duck.com>
2024-12-30 21:04:35 +0800

    Update

--- arch/arch.js
+++ arch/arch.js
@@ -3,75 +3,70 @@ addEventListener("fetch", (event) => {
 });
 
 /**
- * @param {FetchEvent} event
- */
+* @param {FetchEvent} event
+*/
 async function proxy(event) {
   const getReqHeader = (/** @type {string} */ key) =>
     event.request.headers.get(key);
-  let url = new URL(event.request.url);
+  let targetURL = new URL(event.request.url);
 
-  if (url.pathname.startsWith("/archlinux") || url.pathname.startsWith("/icons") || url.pathname === "/" ) {
+  if (targetURL.pathname.startsWith("/archlinux") || targetURL.pathname.includes(".") || targetURL.pathname === "/") {
     const country = event.request.cf.country;
     const colo = event.request.cf.colo;
 
     if (country === "US") {
-      url.hostname = "arch.hu.fo";
-    } else if (
-      colo === "NRT" ||
-      colo === "KIX" ||
-      colo === "FUK" ||
-      colo === "OKA"
-    ) {
-      url.hostname = "mirror.nishi.network";
+      targetURL.hostname = "mirrors.ocf.berkeley.edu";
+    } else if (country === "JP") {
+      targetURL.hostname = "repo.jing.rocks";
     } else if (country === "HK" || colo === "HKG") {
-      url.hostname = "mirror-hk.koddos.net";
+      targetURL.hostname = "mirror-hk.koddos.net";
     } else if (country === "CA") {
-      url.hostname = "mirror.cedille.club";
+      targetURL.hostname = "mirror.cedille.club";
     } else {
-      const mirrors = [
-        "arch.hu.fo",
-        "mirrors.sonic.net",
-        "mirror.lty.me",
-        "mirrors.xtom.com",
-      ];
-      var randomMirror = mirrors[Math.floor(Math.random() * mirrors.length)];
-      url.hostname = randomMirror;
+      targetURL.hostname = "mirrors.sonic.net";
     }
 
     let parameter = {
       headers: {
-        Host: url.hostname,
+        "Host": targetURL.hostname,
         "User-Agent": getReqHeader("User-Agent"),
-        Accept: getReqHeader("Accept"),
-        "Accept-Language": getReqHeader("Accept-Language"),
+        "Accept": getReqHeader("Accept"),
         "Accept-Encoding": getReqHeader("Accept-Encoding"),
-        Connection: "keep-alive",
+        "Connection": "keep-alive",
         "Cache-Control": "max-age=0",
       },
     };
-
+    if (event.request.headers.has("Range")) {
+      parameter.headers.Range = getReqHeader("Range");
+    }
     if (event.request.headers.has("Referer")) {
-      parameter.headers.Referer = getReqHeader("Referer");
+      parameter.headers.Referer = targetURL.toString();
     }
 
-    if (event.request.headers.has("Origin")) {
-      parameter.headers.Origin = getReqHeader("Origin");
+    const response = await fetch(new Request(targetURL, event.request), parameter);
+
+    if (response.status === 301) {
+      const location = response.headers.get("Location");
+      const clientURL = new URL(event.request.url);
+      const redirectURL = new URL(location);
+      redirectURL.hostname = clientURL.hostname;
+      const newResponse = new Response(response.body, response);
+      newResponse.headers.set("Location", redirectURL.toString());
+      return newResponse;
     }
 
-    const response = await fetch(new Request(url, event.request), parameter);
-
-    if (response.headers.get("Content-Type").includes("text/html")) {
+    if (response.headers.has("Content-Type") && response.headers.get("Content-Type").includes("text/html")) {
       const openGraphMetaTags = `
-    <meta property="og:url" content="https://arch.akihi.me">
+    <meta property="og:url" content="https://arch.boletus.me">
     <meta property="og:type" content="website">
-    <meta property="og:title" content="arch.akihi.me" />
+    <meta property="og:title" content="arch.boletus.me" />
     <meta property="og:description" content="Arch Linux mirror" />
     <meta property="og:image" content="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons@master/png/arch.png" />
     <meta property="og:image:width" content="64" />
     <meta property="og:image:height" content="64" />
-    <meta property="twitter:domain" content="arch.akihi.me">
-    <meta property="twitter:url" content="https://arch.akihi.me">
-    <meta name="twitter:title" content="arch.akihi.me">
+    <meta property="twitter:domain" content="arch.boletus.me">
+    <meta property="twitter:url" content="https://arch.boletus.me">
+    <meta name="twitter:title" content="arch.boletus.me">
     <meta name="twitter:description" content="Arch Linux mirror">
     <meta name="twitter:image" content="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons@master/png/arch.png">
     `;
@@ -88,11 +83,11 @@ async function proxy(event) {
         })
         .on("body", {
           element(element) {
-            if (url.pathname === "/archlinux/") {
+            if (targetURL.pathname === "/archlinux/") {
               element.append(
                 `
               <div style="position: relative; bottom: 5px;font-size: 16px; text-align: left;">
-              <p>Country: ${country}, Colo: ${colo}, Mirror: ${url.hostname}</p>
+              <p>Country: ${country}, Colo: ${colo}, Mirror: ${targetURL.hostname}</p>
               </div>
             `,
                 { html: true }
--- debian/debian.js
+++ debian/debian.js
@@ -9,118 +9,91 @@ async function proxy(event) {
   const getReqHeader = (/** @type {string} */ key) =>
     event.request.headers.get(key);
 
-  let url = new URL(event.request.url);
+  let targetURL = new URL(event.request.url);
   const country = event.request.cf.country;
   const colo = event.request.cf.colo;
 
   if (country === "HK" || colo === "HKG") {
-    url.hostname = "mirror.xtom.com.hk";
+    targetURL.hostname = "cdn-fastly.deb.debian.org";
   } else {
-    url.hostname = "deb.debian.org";
+    targetURL.hostname = "deb.debian.org";
   }
-
-  let UA = getReqHeader("User-Agent").toLowerCase();
 
   let parameter = {
     headers: {
-      Host: url.hostname,
+      "Host": targetURL.hostname,
       "User-Agent": getReqHeader("User-Agent"),
-      Accept: getReqHeader("Accept"),
-      "Accept-Language": getReqHeader("Accept-Language"),
+      "Accept": getReqHeader("Accept"),
       "Accept-Encoding": getReqHeader("Accept-Encoding"),
-      Connection: "keep-alive",
+      "Connection": "keep-alive",
       "Cache-Control": "max-age=0",
     },
   };
-
+  if (event.request.headers.has("Range")) {
+    parameter.headers.Range = getReqHeader("Range");
+  }
   if (event.request.headers.has("Referer")) {
-    parameter.headers.Referer = getReqHeader("Referer");
+    parameter.headers.Referer = targetURL.toString();
   }
 
-  if (event.request.headers.has("Origin")) {
-    parameter.headers.Origin = getReqHeader("Origin");
+  const response = await fetch(new Request(targetURL, event.request), parameter);
+
+  if (response.status === 301) {
+    const location = response.headers.get("Location");
+    const clientURL = new URL(event.request.url);
+    const redirectURL = new URL(location);
+    redirectURL.hostname = clientURL.hostname;
+    const newResponse = new Response(response.body, response);
+    newResponse.headers.set("Location", redirectURL.toString());
+    return newResponse;
   }
-  if (
-    !(
-      UA.includes("apt") ||
-      UA.includes("curl") ||
-      UA.includes("wget") ||
-      (url.pathname.includes(".") && !url.pathname.endsWith("/"))
-    )
-  ) {
-    if (event.request.url === "https://deb.akihi.me/debian") {
-      return new Response(null, {
-        status: 301,
-        headers: {
-          Location: "https://deb.akihi.me/debian/",
-        },
-      });
-    }
 
-    const proxyRequest = new Request(url, {
-      method: event.request.method,
-      headers: event.request.headers,
-      body: event.request.body,
-    });
+  if (response.headers.has("Content-Type") && response.headers.get("Content-Type").includes("text/html")) {
 
-    try {
-      const proxyResponse = await fetch(proxyRequest);
-
-      const clonedResponse = new Response(proxyResponse.body, {
-        status: proxyResponse.status,
-        statusText: proxyResponse.statusText,
-        headers: proxyResponse.headers,
-      });
-
-      const openGraphMetaTags = `
-      <meta property="og:title" content="deb.akihi.me" />
+    const openGraphMetaTags = `
+      <meta property="og:title" content="deb.boletus.me" />
       <meta property="og:description" content="Debian mirrors backed by Cloudflare CDN" />
       <meta property="og:image" content="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons@master/png/debian.png" />
       <meta property="og:image:width" content="64" />
       <meta property="og:image:height" content="64" />
-      <meta name="twitter:title" content="deb.akihi.me">
+      <meta name="twitter:title" content="deb.boletus.me">
       <meta name="twitter:description" content="Debian mirrors backed by Cloudflare CDN">
       <meta name="twitter:image" content="https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons@master/png/debian.png">
       `;
-      const modifiedResponse = new HTMLRewriter()
-        .on("head", {
-          element(element) {
-            element.append(
-              "<title>Debian mirrors backed by Cloudflare CDN</title>" +
-                '<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAACQElEQVRYw9WWsY7TQBCGv0U0AZkqdpPihJCbVER5ALu9/uLXQfc64D6t/QARnZsIEEWaTSp8IeVSJI7s9e7aPpNDTGnvzvzzz8w/K3iGrQnVhBkAJ3bcsQVWnPjOExsRDfAlhgTOiFXfsyd2HFmIBz6PB1DP+DkWk1njdAIwZR3gM+eL9a4JsA2EE4AeXJANqq9+3wTile3yhmXj8oFgUHBTwDVhi83X+occiIASDzg30z3bQc2qs6YuTJzLsnUzENWoGxu88lcHk7sYSEmYIq80dQVPSXjL10bDmfrEo6TEwwdVkAhqo9kqQUCGJObEzplZDiikEmxFpNGaklCf/x98EFOk+knY8tMqgSQG4E1H9r8Jla0xfU186mD0fw0A7/l2pd/V8SkJAe+wqZzt7oQZexI7gCc2vRpuilTLjrM5j31ctXsAcNb/XPtui/hk9PtAZmfAZ6UA7vjYC70LpMmOLNxKuL/o+96h85WlWi2biTRJMimgEYDOhMkizlTW9aLKurgEntd2TErChBkHAuNqNgKQ7J3ZH1mIE7vGvoi0wFXwKVIF+NaJaQDou2yqWZb8Yk2o9JqnJGTEaopUBwLhWt2tHxuWqsRzPiIqK+Cqbvr+9yjpGlVjCaot2Of5NQdxz1YcWYgAH4+SA4GIyUSf4FbLiNWQ998YMzZh1yK6OYBqDb8EC9YnWYAPQOHQhJsCqEanSxNubi/ZkE4QLj3/r5kYJBYFKyXZ/5XX8igrWP37vsh5ZE2oClYqH+HnD8KP16YBvW+JAAAAAElFTkSuQmCC" />',
-              { html: true }
-            );
-            element.append(openGraphMetaTags, { html: true });
-          },
-        })
-        .on("title, h1", {
-          element(element) {
-            if (event.request.url === "https://deb.akihi.me/") {
-              const tagName = element.tagName.toLowerCase();
-              if (tagName === "title" || tagName === "h1") {
-                element.setInnerContent("Debian mirrors backed by ");
-                if (tagName === "title") {
-                  element.append("Cloudflare CDN", { html: true });
-                } else {
-                  element.append("<s>Fastly</s> Cloudflare CDN", {
-                    html: true,
-                  });
-                }
+    const modifiedResponse = new HTMLRewriter()
+      .on("head", {
+        element(element) {
+          element.append(
+            "<title>Debian mirrors backed by Cloudflare CDN</title>" +
+            '<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABmJLR0QA/wD/AP+gvaeTAAACQElEQVRYw9WWsY7TQBCGv0U0AZkqdpPihJCbVER5ALu9/uLXQfc64D6t/QARnZsIEEWaTSp8IeVSJI7s9e7aPpNDTGnvzvzzz8w/K3iGrQnVhBkAJ3bcsQVWnPjOExsRDfAlhgTOiFXfsyd2HFmIBz6PB1DP+DkWk1njdAIwZR3gM+eL9a4JsA2EE4AeXJANqq9+3wTile3yhmXj8oFgUHBTwDVhi83X+occiIASDzg30z3bQc2qs6YuTJzLsnUzENWoGxu88lcHk7sYSEmYIq80dQVPSXjL10bDmfrEo6TEwwdVkAhqo9kqQUCGJObEzplZDiikEmxFpNGaklCf/x98EFOk+knY8tMqgSQG4E1H9r8Jla0xfU186mD0fw0A7/l2pd/V8SkJAe+wqZzt7oQZexI7gCc2vRpuilTLjrM5j31ctXsAcNb/XPtui/hk9PtAZmfAZ6UA7vjYC70LpMmOLNxKuL/o+96h85WlWi2biTRJMimgEYDOhMkizlTW9aLKurgEntd2TErChBkHAuNqNgKQ7J3ZH1mIE7vGvoi0wFXwKVIF+NaJaQDou2yqWZb8Yk2o9JqnJGTEaopUBwLhWt2tHxuWqsRzPiIqK+Cqbvr+9yjpGlVjCaot2Of5NQdxz1YcWYgAH4+SA4GIyUSf4FbLiNWQ998YMzZh1yK6OYBqDb8EC9YnWYAPQOHQhJsCqEanSxNubi/ZkE4QLj3/r5kYJBYFKyXZ/5XX8igrWP37vsh5ZE2oClYqH+HnD8KP16YBvW+JAAAAAElFTkSuQmCC" />',
+            { html: true }
+          );
+          element.append(openGraphMetaTags, { html: true });
+        },
+      })
+      .on("title, h1", {
+        element(element) {
+          if (event.request.url === "https://deb.boletus.me/") {
+            const tagName = element.tagName.toLowerCase();
+            if (tagName === "title" || tagName === "h1") {
+              element.setInnerContent("Debian mirrors backed by ");
+              if (tagName === "title") {
+                element.append("Cloudflare CDN", { html: true });
+              } else {
+                element.append("<s>Fastly</s> Cloudflare CDN", {
+                  html: true,
+                });
               }
             }
-          },
-        })
-        .transform(clonedResponse);
+          }
+        },
+      })
+      .transform(response);
 
-      return new Response(modifiedResponse.body, {
-        status: clonedResponse.status,
-        statusText: clonedResponse.statusText,
-        headers: clonedResponse.headers,
-      });
-    } catch (error) {
-      return new Response("Error: ${error.message}, { status: 500 }");
-    }
-  } else {
-    return fetch(new Request(url, event.request), parameter);
-  }
+    return new Response(modifiedResponse.body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+    });
+  } return response;
 }
